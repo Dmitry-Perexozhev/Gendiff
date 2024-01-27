4e57361535c38abad441a0a285be1527cb16b2fe
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-01-27 14:54:36 +0300

    Add lint github action

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Dmitry-Perexozhev/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Dmitry-Perexozhev/python-project-50/actions)
+
+### Flat json file comparison
+(https://asciinema.org/a/DhuDXtWSeVr3SdvTUgtov10n7)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,33 +2,43 @@
 import json
 import argparse
 
+
 def generate_diff(path_to_file1, path_to_file2):
     with open(path_to_file1) as file1, open(path_to_file2) as file2:
         data1 = json.load(file1)
         data2 = json.load(file2)
         keys = sorted(set(data1.keys()) | set(data2.keys()))
-        print('{')
+        dif_stroke = ''
         for key in keys:
             if key in data1 and key in data2:
                 if data1.get(key) == data2.get(key):
-                    print(f'    {key}: {data1.get(key)}')
+                    dif_stroke += f'  {key}: {data1.get(key)}\n'
                 else:
-                    print(f'  - {key}: {data1.get(key)}')
-                    print(f'  + {key}: {data2.get(key)}')
+                    dif_stroke += f'- {key}: {data1.get(key)}\n'
+                    dif_stroke += f'+ {key}: {data2.get(key)}\n'
             elif key in data1:
-                print(f'  - {key}: {data1.get(key)}')
+                dif_stroke += f'- {key}: {data1.get(key)}\n'
             else:
-                print(f'  + {key}: {data2.get(key)}')
-        print('}')
+                dif_stroke += f'+ {key}: {data2.get(key)}\n'
+        return dif_stroke
 
-def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str, help='set format of output')
     args = parser.parse_args()
-    generate_diff(args.first_file, args.second_file)
+    print('{')
+    for stroke in generate_diff(args.first_file,
+                                args.second_file).split('\n')[:-1]:
+        print('  ', stroke)
+    print('}')
 
+
+def main():
+    init_argparse()
 
 
 if __name__ == '__main__':
