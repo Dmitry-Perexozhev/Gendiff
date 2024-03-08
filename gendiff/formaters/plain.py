ce01e53bbc52b189513d4bdfce41b0f5a1c1ec21
Dmitry_Perexozhev <perexozhev99@gmail.com>
2024-03-08 12:57:36 +0300

    Remove nonlocal diff_str

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -14,19 +14,18 @@ def is_correct_output(obj):
 def gen_plain_format(path_to_file1, path_to_file2):
     data_of_file1, data_of_file2 = load_file(path_to_file1, path_to_file2)  # загрузка данных из файлов
     diffs = generate_diff(data_of_file1, data_of_file2)
-    dif_stroke = ''
 
     def recursive_plain_format(dict_diffs, path):
-        nonlocal dif_stroke
+        result = []
         for dif in dict_diffs:
             if dif['status'] == 'has_children':
-                recursive_plain_format(dif["value"], path+[dif["key"]])
+                result += recursive_plain_format(dif["value"], path+[dif["key"]])
             elif dif['status'] == 'added':
-                dif_stroke += f'Property \'{".".join(path+[dif["key"]])}\' was added with value: {is_correct_output(dif["value"])}\n'
+                result.append(f'Property \'{".".join(path+[dif["key"]])}\' was added with value: {is_correct_output(dif["value"])}')
             elif dif['status'] == 'removed':
-                dif_stroke += f'Property \'{".".join(path+[dif["key"]])}\' was removed\n'
+                result.append(f'Property \'{".".join(path+[dif["key"]])}\' was removed')
             elif dif['status'] == 'updated':
-                dif_stroke += f'Property \'{".".join(path+[dif["key"]])}\' was updated. From {is_correct_output(dif["old_value"])} to {is_correct_output(dif["new_value"])}\n'
-    recursive_plain_format(diffs, [])
-    return dif_stroke[:-1]
-
+                result.append(f'Property \'{".".join(path+[dif["key"]])}\' was updated. From {is_correct_output(dif["old_value"])} to {is_correct_output(dif["new_value"])}')
+        return result
+    return '\n'.join(recursive_plain_format(diffs, []))
+#print(gen_plain_format('file1_recursive.json', 'file2_recursive.json'))
--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -14,51 +14,51 @@ def is_correct_output(obj):
 
 def gen_stylish_format(path_to_file1, path_to_file2):
     data_of_file1, data_of_file2 = load_file(path_to_file1, path_to_file2)  # загрузка данных из файлов
-    dif_stroke = '{\n'
     diffs = generate_diff(data_of_file1, data_of_file2)
 
     def recursive_stylish_format(dict_diffs, depth):
-        nonlocal dif_stroke
+        result = []
         indent = REPLACER * (NUMBER_OF_INDENTS * depth - 2)
         for dif in dict_diffs:
             if 'status' in dif:
                 if dif['status'] == 'unchanged':
-                    dif_stroke += f'{indent}  {dif["key"]}: {is_correct_output(dif["value"])}\n'
+                    result.append(f'{indent}  {dif["key"]}: {is_correct_output(dif["value"])}\n')
                 elif dif['status'] == 'added':
                     if isinstance(dif['value'], dict):
-                        dif_stroke += f'{indent}+ {dif["key"]}: {{\n'
-                        recursive_stylish_format(dif['value'], depth + 1)
+                        result.append(f'{indent}+ {dif["key"]}: {{\n')
+                        result += recursive_stylish_format(dif['value'], depth + 1)
                     else:
-                        dif_stroke += f'{indent}+ {dif["key"]}: {is_correct_output(dif["value"])}\n'
+                        result.append(f'{indent}+ {dif["key"]}: {is_correct_output(dif["value"])}\n')
                 elif dif['status'] == 'removed':
                     if isinstance(dif['value'], dict):
-                        dif_stroke += f'{indent}- {dif["key"]}: {{\n'
-                        recursive_stylish_format(dif['value'], depth + 1)
+                        result.append(f'{indent}- {dif["key"]}: {{\n')
+                        result += recursive_stylish_format(dif['value'], depth + 1)
                     else:
-                        dif_stroke += f'{indent}- {dif["key"]}: {is_correct_output(dif["value"])}\n'
+                        result.append(f'{indent}- {dif["key"]}: {is_correct_output(dif["value"])}\n')
                 elif dif['status'] == 'has_children':
-                    dif_stroke += f'{indent}  {dif["key"]}: {{\n'
-                    recursive_stylish_format(dif["value"], depth+1)
+                    result.append(f'{indent}  {dif["key"]}: {{\n')
+                    result += recursive_stylish_format(dif["value"], depth+1)
                 elif dif['status'] == 'updated':
                     if isinstance(dif['old_value'], dict):
-                        dif_stroke += f'{indent}- {dif["key"]}: {{\n'
-                        recursive_stylish_format(dif['old_value'], depth + 1)
+                        result.append(f'{indent}- {dif["key"]}: {{\n')
+                        result += recursive_stylish_format(dif['old_value'], depth + 1)
                     else:
-                        dif_stroke += f'{indent}- {dif["key"]}: {is_correct_output(dif["old_value"])}\n'
+                        result.append(f'{indent}- {dif["key"]}: {is_correct_output(dif["old_value"])}\n')
                     if isinstance(dif['new_value'], dict):
-                        dif_stroke += f'{indent}+ {dif["key"]}: {{\n'
-                        recursive_stylish_format(dif['new_value'], depth + 1)
+                        result.append(f'{indent}+ {dif["key"]}: {{\n')
+                        result += recursive_stylish_format(dif['new_value'], depth + 1)
                     else:
-                        dif_stroke += f'{indent}+ {dif["key"]}: {is_correct_output(dif["new_value"])}\n'
+                        result.append(f'{indent}+ {dif["key"]}: {is_correct_output(dif["new_value"])}\n')
             else:
                 if isinstance(dict_diffs[dif], dict):
-                    dif_stroke += f'{indent}  {dif}: {{\n'
-                    recursive_stylish_format(dict_diffs[dif], depth + 1)
+                    result.append(f'{indent}  {dif}: {{\n')
+                    result += recursive_stylish_format(dict_diffs[dif], depth + 1)
                 else:
-                    dif_stroke += f'{indent}  {dif}: {is_correct_output(dict_diffs[dif])}\n'
-        dif_stroke += f'{indent[:-2]}}}\n'
-    recursive_stylish_format(diffs, 1)
-    dif_stroke = dif_stroke[:-2] + '}'
+                    result.append(f'{indent}  {dif}: {is_correct_output(dict_diffs[dif])}\n')
+        result.append(f'{indent[:-2]}}}\n')
+        return result
+    return '{\n'+''.join(recursive_stylish_format(diffs, 1))[:-1]
 
-    return dif_stroke
+
+#print(gen_stylish_format('file1.json', 'file2.json'))
 
